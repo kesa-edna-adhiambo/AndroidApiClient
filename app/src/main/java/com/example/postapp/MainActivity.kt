f99fb735145806e32372dead40002bca072a6224
kesa-edna-adhiambo <edna@gmail.com>
2024-07-24 09:55:57 +0300

    Fetched Api

--- app/src/main/java/com/example/postapp/MainActivity.kt
+++ app/src/main/java/com/example/postapp/MainActivity.kt
@@ -4,34 +4,44 @@ import ApiClient
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.postapp.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var recyclerView: RecyclerView
+    lateinit var postAdapter: PostAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) // Initialize the binding
+        setContentView(binding.root)
         fetchPosts()
+        recyclerView = binding.rvApi
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
-
-    fun fetchPosts(){
+    fun fetchPosts() {
         val apiInterface = ApiClient.buildApiInterface(PostApiInterface::class.java)
         val request = apiInterface.getPosts()
         request.enqueue(object : Callback<List<Post>> {
-            override fun onResponse(p0: Call<List<Post>>, p1: Response<List<Post>>) {
-               if(p1.isSuccessful){
-                   val posts = p1.body()
-                   Toast.makeText(baseContext, "Fetched ${posts!!.size} post", Toast.LENGTH_LONG).show()
-               }
+            override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
+                if (response.isSuccessful) {
+                    val posts = response.body()
+                    posts?.let {
+                        postAdapter = PostAdapter(it)
+                        recyclerView.adapter = postAdapter
+                    }
+                    Toast.makeText(baseContext, "Fetched ${posts!!.size} posts", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(baseContext, response.errorBody()?.string(), Toast.LENGTH_LONG).show()
+                }
             }
-
-            override fun onFailure(p0: Call<List<Post>>, p1: Throwable) {
-                Toast.makeText(baseContext,p1.message.toString(), Toast.LENGTH_LONG).show()
+            override fun onFailure(call: Call<List<Post>>, t: Throwable) {
+                Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
             }
-
         })
-
-
     }
 }
